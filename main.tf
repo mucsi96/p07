34e672aa5c6893245ae6346adecc5e74efad5887
Igor Bari <mucsi96@gmail.com>
2026-06-16 16:40:44 +0200

    feat: update module source reference to version v-39 and refine destroy script

--- main.tf
+++ main.tf
@@ -143,7 +143,7 @@ locals {
   client_log_url = "https://${local.faro_hostname}/collect"
 
   module_source_base = "git::https://github.com/mucsi96/k8s-modules.git//modules"
-  module_source_ref  = "27a444c"
+  module_source_ref  = "v-39"
 
   oauth2_proxy_chart_version = "10.4.3"  #https://github.com/oauth2-proxy/manifests/releases
   oauth2_proxy_image_version = "v7.15.2" #https://github.com/oauth2-proxy/oauth2-proxy/releases
--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -5,5 +5,3 @@ set -euo pipefail
 source .venv/bin/activate
 
 terraform destroy -refresh=false -auto-approve -target=module.provision_hetzner_server
-
-# terraform destroy -refresh=false -auto-approve
