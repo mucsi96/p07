6ad220456e4e251bed2f5a5b7fc8241e0eae2175
Igor Bari <mucsi96@gmail.com>
2026-08-31 13:25:21 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dev shell for p07 — Hetzner Cloud MicroK8s environment tooling";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
